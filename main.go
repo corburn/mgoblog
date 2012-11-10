b7bc610b8427f9a7ca41dbd83502a6e8f9b578b0
Jason Travis <infomaniac7@gmail.com>
2012-11-09 18:06:05 -0700

    add: Placeholder for root http handler.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"labix.org/v2/mgo"
 )
 
@@ -13,10 +16,17 @@ const (
 
 var session *mgo.Session
 
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a place holder for the blog")
+}
+
 func main() {
 	session, err := mgo.Dial(url)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
+
+	http.HandleFunc("/", root)
+	http.ListenAndServe(":8080", nil)
 }
