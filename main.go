e2d310be875a3071629c12ac51dd3d9feb6da010
Jason Travis <infomaniac7@gmail.com>
2012-11-10 17:24:31 -0700

    add: signup handler and signup.html template.
    The signup handler registers new users. It returns the signup.html
    template and retrieves the username, password, and email form values.
    Nothing is stored in the databse. The password is transmitted in plain
    text. TLS should be implemented. The input is not validated and the
    second password field is ignored.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 
 	"labix.org/v2/mgo"
 )
@@ -12,12 +13,31 @@ const (
 	database = "blog"
 	sessions = "sessions"
 	users    = "users"
+
+	tmplDir = "tmpl/"
 )
 
-var session *mgo.Session
+var (
+	session *mgo.Session
+	templates = template.Must(template.ParseFiles(tmplDir+"signup.html"))
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string, u *User) {
+	if err := templates.ExecuteTemplate(w, tmpl, u); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is a place holder for the blog")
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	email := r.FormValue("email")
+	user := &User{Id: username, Password: []byte(password), Email: email}
+	renderTemplate(w, "signup.html", user)
 }
 
 func main() {
@@ -28,5 +48,6 @@ func main() {
 	defer session.Close()
 
 	http.HandleFunc("/", root)
+	http.HandleFunc("/signup", signup)
 	http.ListenAndServe(":8080", nil)
 }
